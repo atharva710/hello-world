026db9b86f51dcc58bf7f3f3aa2a2d0e49ee7454
atharva710 <54885087+atharva710@users.noreply.github.com>
2019-09-04 13:23:37 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void i_s(int data,int position)
+{	 int i
+ 	struct node *p=head;	//temperory pointer for traverse
+ 	struct node *new=(struct node)malloc(sizeof(struct node));
+	new->data=data;
+	if(position==1)		//if node inserted at first position
+	{		
+			new->next=p;
+			head=new;
+	}
+	for(i=1;i<position-1;i++)	//traverse the position-1 node
+	{
+		p=p->next;
+	}
+	new->next=p->next;	//new node 
+	p->next=new;
+}
+
